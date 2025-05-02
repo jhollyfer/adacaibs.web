3a9afad71a5c8a35e85c7e57e6e40d6c7aff7155
indio <jhollyferr@gmail.com>
2025-05-02 17:12:45 -0500

    fix

--- src/pages/landing-page/home/index.tsx
+++ src/pages/landing-page/home/index.tsx
@@ -4,7 +4,6 @@ import { Contact } from "./components/contact";
 import { Feature } from "./components/feature";
 import { Hero } from "./components/hero";
 import { Notice } from "./components/notice";
-import { Testimonial } from "./components/testimonial";
 
 export function Home(): React.JSX.Element {
   return (
@@ -13,7 +12,7 @@ export function Home(): React.JSX.Element {
       <Feature />
       <About />
       <Notice />
-      <Testimonial />
+      {/* <Testimonial /> */}
       <Contact />
     </React.Fragment>
   );
